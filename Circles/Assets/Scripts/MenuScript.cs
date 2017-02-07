5f538ec3c99cf322d846f03a075d57e346c2c30c
Jenna <jenna.galka@gmail.com>
2017-02-06 20:31:52 -0500

    Start Menu
    yay

---
+++ Circles/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button startText;
+
+	// Use this for initialization
+	void Start () {
+
+		startText = startText.GetComponent<Button> ();
+		
+	}
+
+	public void StartLevel(){
+
+		SceneManager.LoadScene (1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
